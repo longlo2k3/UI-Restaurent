5135a00e6318bbcfb9b1161715257641f07e0406
longlo2k3 <168855062+longlo2k3@users.noreply.github.com>
2024-08-24 20:12:23 +0700

    commit

---
+++ restaurent-object/src/main.jsx
@@ -0,0 +1,18 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
